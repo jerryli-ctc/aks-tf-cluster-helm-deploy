02756f5f6ff0bba0ba5ebd7e55b394f185d71898
Jerry Li <jerry.li@cantire.com>
2021-08-31 23:31:49 -0400

    fix module

--- output.tf
+++ output.tf
@@ -1,15 +1,20 @@
+
 output "host" {
-  value = azurerm_kubernetes_cluster.default.kube_config.0.host
+  value       = try(module.aks.host, null)
+  description = "The Kubernetes cluster server host"
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
+  value       = try(module.aks.client_key, null)
+  description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster"
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
+  value       = try(module.aks.client_certificate, null)
+  description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster"
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.default.kube_config_raw
+  value       = try(module.aks.kube_config_raw, null)
+  description = "kubeconfig yaml file"
 }
